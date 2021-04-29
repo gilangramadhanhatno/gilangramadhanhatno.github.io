a57674707349839a4c80fb5faa5f4626f2283786
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-04-29 14:16:15 +0700

    menambahkan text-plugin GSAP pada paragraf text di hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,14 @@ const header = document.querySelector(".header");
 menuToggle.addEventListener("click", function () {
   header.classList.toggle("slide");
 });
+
+// GSAP
+const words = ["Front End Developer", "UI/UX Designer"];
+
+let masterTL = gsap.timeline({ repeat: -1 });
+
+words.forEach((word) => {
+  let tl = gsap.timeline({ repeat: 1.1, yoyo: true, repeatDelay: 1 });
+  tl.to(".text-plugin", { duration: 1, text: word });
+  masterTL.add(tl);
+});
